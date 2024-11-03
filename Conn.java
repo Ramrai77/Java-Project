7826009daf2da1e79b35d8fad21695059311c2c0
Ramrai77 <ramrrai298@gmail.com>
2024-11-03 18:10:00 +0530

    Update Conn.java

--- Conn.java
+++ Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelmanagementsystem", "root", "dibyanshu#75@18ram");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelmanagementsystem", "root", "databasepassword");
             s = c.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
